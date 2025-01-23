51c5e5116e370cd4819bc4d2fc4d6f7edb81d335
Thirugnanam N <23csb53@karpagamtech.ac.in>
2025-01-23 06:53:16 +0000

    Sync LeetCode submission - Kth Missing Positive Number (java)

---
+++ problems/kth_missing_positive_number/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int current = 1;
+        int i = 0;       
+        while (true) {
+            if (i < arr.length && arr[i] == current) {
+                i++;
+            } else {
+                k--;
+                if (k == 0) {
+                    return current;
+                    }
+            }
+            current++;
+        }
+    }
+}
