cb702fa2e58cd19c4338afe5e648cc73daadc5aa
Thirugnanam N <23csb53@karpagamtech.ac.in>
2025-02-15 06:36:50 +0000

    Sync LeetCode submission - Flipping an Image (java)

---
+++ problems/flipping_an_image/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[][] flipAndInvertImage(int[][] image) {
+        for (int[] row : image) {
+            for (int i = 0; i < (row.length + 1) / 2; i++) {
+                int temp = row[i] ^ 1; 
+                row[i] = row[row.length - 1 - i] ^ 1;
+                row[row.length - 1 - i] = temp;
+            }
+        }
+        return image;
+    }
+}
+    
