08bab46105478ee1119d5676c1049e1d1cef1860
Thirugnanam N <23csb53@karpagamtech.ac.in>
2025-03-07 09:32:13 +0000

    Sync LeetCode submission - Rotate String (java)

--- problems/rotate_string/solution.java
+++ problems/rotate_string/solution.java
@@ -1,9 +1,6 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        if(s.length()!=goal.length()) {
-            return false;
-            }
-            return(s+ s).contains(goal);
+        if(s.length()!=goal.length()) return false;
+        return (s+s).contains(goal);
     }
-        
 }
