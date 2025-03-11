26f8c15f31d666e0b084edefe9ab1d23fbfe6fea
Thirugnanam N <23csb53@karpagamtech.ac.in>
2025-03-11 10:20:27 +0000

    Sync LeetCode submission - Fibonacci Number (java)

---
+++ problems/fibonacci_number/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int n) {  
+       if (n <= 1) return n;
+            int first=0, second=1;
+            for(int i=2;i<=n;i++){
+                int third= second;
+                second=first+second;
+                first = third;
+            }
+            return second;
+    }
+}
