4ba7d9fa5b09d23ca008b24b6d0264928aed6e34
Thirugnanam N <23csb53@karpagamtech.ac.in>
2025-04-04 06:37:59 +0000

    Sync LeetCode submission - Check If N and Its Double Exist (java)

---
+++ problems/check_if_n_and_its_double_exist/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean checkIfExist(int[] arr) {
+        int n=arr.length;
+        boolean flag=false;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+            if(arr[i]==2*arr[j] || arr[j]==2*arr[i]){
+                flag= true;
+                break;
+        }
+            }
+        }
+        return  flag;
+    }
+}
