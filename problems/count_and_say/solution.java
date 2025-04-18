857d31e18703ea86508011f46305406414e48f04
Thirugnanam N <23csb53@karpagamtech.ac.in>
2025-04-18 03:40:27 +0000

    Sync LeetCode submission - Count and Say (java)

--- problems/count_and_say/solution.java
+++ problems/count_and_say/solution.java
@@ -1,37 +1,20 @@
 class Solution {
     public String countAndSay(int n) {
-        // Handle edge case: if n <= 0, return empty string
-        if(n <= 0){ return "" ;}
-        // Base case: if n == 1, return "1"
-        else if(n == 1){ return "1"; }
-        // Initialize starting string as "1"
-        String s = "1";
-        // Counter to track number of iterations
-        int count = 1;
-        // Loop until we reach the nth term
-        while(count != n){
-            s = RLE(s);
-            count++;
-        }
-        return s;
-    }
-    
-    public String RLE(String a){
-        // Initialize result string for the next term
-        String result = "";
-        // i tracks the start of current digit group, j scans ahead, count tracks occurrences
-        int i = 0; int j = 0; int count = 0;
-        // Process the entire string
-        while(j < a.length()){
-            // If current and next characters match, increment count and move j
-            if(a.charAt(i) == a.charAt(j)){ count++; j++; }
-            // When digits differ or end is reached, append count and digit to result
-            if(j == a.length() || a.charAt(i) != a.charAt(j)){
-                String temp = "" + count + a.charAt(i);
-                result = result + temp;
-                i = j; count = 0;
+        String res = "1";
+        for (int i = 1; i < n; i++) {
+            StringBuilder temp = new StringBuilder();
+            int count = 1;
+            for (int j = 1; j < res.length(); j++) {
+                if (res.charAt(j) == res.charAt(j - 1)) {
+                    count++;
+                } else {
+                    temp.append(count).append(res.charAt(j - 1));
+                    count = 1;
+                }
             }
+            temp.append(count).append(res.charAt(res.length() - 1));
+            res = temp.toString();
         }
-        return result;
+        return res;
     }
 }
