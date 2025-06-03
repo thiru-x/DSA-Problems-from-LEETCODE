6141fae2f487f9e332ada22797f9e2607654461f
Thirugnanam N <23csb53@karpagamtech.ac.in>
2025-06-03 15:11:50 +0000

    Sync LeetCode submission - Gray Code (java)

---
+++ problems/gray_code/solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public List<Integer> grayCode(int n) {
+        int size = 1 << n;  // 2^n
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < size; i++) {
+            result.add(i ^ (i >> 1));
+        }
+        return result;
+    }
+}
