b82feaab85a47049e6a48ce5a849f7184540c4e1
Thirugnanam N <23csb53@karpagamtech.ac.in>
2025-06-19 08:55:18 +0000

    Sync LeetCode submission - Partition Array Such That Maximum Difference Is K (java)

---
+++ problems/partition_array_such_that_maximum_difference_is_k/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int partitionArray(int[] nums, int k) {
+        Arrays.sort(nums);
+        int ans=1;
+        int min=nums[0];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]-min > k){
+                ans++;
+                min=nums[i];
+            }
+        }
+        return ans;
+
+    }
+}
