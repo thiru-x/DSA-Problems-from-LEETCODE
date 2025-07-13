fbee3d4911b60434c75ad533cb59f76c617e7e60
Thirugnanam N <23csb53@karpagamtech.ac.in>
2025-07-13 02:04:01 +0000

    Sync LeetCode submission - Maximum Matching of Players With Trainers (java)

--- problems/maximum_matching_of_players_with_trainers/solution.java
+++ problems/maximum_matching_of_players_with_trainers/solution.java
@@ -1,11 +1,23 @@
 class Solution {
     public int matchPlayersAndTrainers(int[] players, int[] trainers) {
-        Arrays.sort(players);
-        Arrays.sort(trainers);
-        int i = 0;
-        for(int j=0;i<players.length && j<trainers.length;j++) {
-            if(players[i]<=trainers[j]) i++;
+       Arrays.sort(players);
+       Arrays.sort(trainers);
+
+       int count=0;
+
+       int i=0;
+       int j=0;
+       while(i<players.length && j<trainers.length){
+        if(players[i] <= trainers[j]){
+
+            count++;
+            i++;
+            j++;
+        }else{
+            j++;
         }
-        return i;
+       }
+    return count;
+
     }
 }
