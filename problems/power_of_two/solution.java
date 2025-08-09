7e30c44ecbf05db24c911f1656e45ca23d95b3a4
Thirugnanam N <23csb53@karpagamtech.ac.in>
2025-08-09 14:50:45 +0000

    Sync LeetCode submission - Power of Two (java)

--- problems/power_of_two/solution.java
+++ problems/power_of_two/solution.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
-        for(int i=0;i<31;i++){
-            int a=(int)Math.pow(2,i);
-            if(a==n) return true;
-        }
-        return false;
+
+       if (n <= 0) return false;
+    while (n % 2 == 0) {
+        n /= 2;
+    }
+    return n == 1;
+
     }
 }
