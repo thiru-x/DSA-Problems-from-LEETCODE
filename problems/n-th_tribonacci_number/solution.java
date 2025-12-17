5a6816876ebac92ed89f74daa2119b2922737489
Thirugnanam N <23csb53@karpagamtech.ac.in>
2025-12-17 10:20:39 +0000

    Sync LeetCode submission - N-th Tribonacci Number (java)

--- problems/n-th_tribonacci_number/solution.java
+++ problems/n-th_tribonacci_number/solution.java
@@ -1,18 +1,20 @@
 class Solution {
-    public int tribonacci(int n) {
-        if(n == 0){
-            return 0;            
+
+     public int solve(int n,int dp[]){
+
+        if(dp[n]!=-1) return dp[n];
+        else if(n==1 || n==2){
+            dp[n]=1;
         }
-        if(n==1 || n==2){
-            return 1;
-        }
-        int[] dp = new int[n+1];
-        dp[0] = 0;
-        dp[1] = 1;
-        dp[2] = 1;
-        for(int i = 3; i <= n ;i++){
-            dp[i] = dp[i-1]+dp[i-2]+dp[i-3];
+        else{
+            dp[n]=solve(n-1,dp)+solve(n-2,dp)+solve(n-3,dp);
         }
         return dp[n];
+    }
+    public int tribonacci(int n) {
+         int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        dp[0]=0;
+        return solve(n,dp);    
     }
 }
