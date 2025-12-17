7cdcfd8ee7442469eaaa4be59fa762cb878a82a4
Thirugnanam N <23csb53@karpagamtech.ac.in>
2025-12-17 10:14:29 +0000

    Sync LeetCode submission - Climbing Stairs (java)

--- problems/climbing_stairs/solution.java
+++ problems/climbing_stairs/solution.java
@@ -1,13 +1,23 @@
-class Solution{
-    public int climbStairs(int n){
-            if (n <= 2) return n;
-            int first=1, second=2;
-            for(int i=3;i<=n;i++){
-                int third=first + second;
-                first = second;
-                second = third;
-            }
-            return second;
+class Solution {
+
+
+    public int solve(int n,int dp[]){
+        if(dp[n]!=-1) return dp[n];
+        if(n==1 || n==2){
+            dp[n]=n;
+        }
+        else{
+            dp[n]=solve(n-1,dp)+solve(n-2,dp);
+        }
+
+        return dp[n];
+    }
+
+
+    public int climbStairs(int n) {
+        int dp[]=new int[n+1];
+        Arrays.fill(dp,-1);
+        dp[0]=0;
+        return solve(n,dp);
     }
 }
-    
