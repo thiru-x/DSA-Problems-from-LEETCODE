892e5f95c734d006de3b9a9fb0f0ab69e12743b9
Thirugnanam N <23csb53@karpagamtech.ac.in>
2026-01-02 10:06:47 +0000

    Sync LeetCode submission - Plus One (java)

--- problems/plus_one/solution.java
+++ problems/plus_one/solution.java
@@ -1,16 +1,14 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        for (int i = digits.length - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
-                digits[i]++;
-                return digits; // Return immediately if no carry is needed
+        for(int i = digits.length - 1; i >= 0; i--) {
+            if(digits[i] != 9) {
+                digits[i] += 1;
+                return digits;
             }
-            digits[i] = 0; // Set to 0 and continue to propagate the carry
-        }
-
-        // If all digits were 9, create a new array with an extra leading 1
-        int[] result = new int[digits.length + 1];
-        result[0] = 1;
-        return result;
+            digits[i] = 0;
+        }   
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
     }
 }
