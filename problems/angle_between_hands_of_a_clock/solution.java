1104590f22bf36b1a93cfb719305f4659670b0d7
Thirugnanam N <23csb53@karpagamtech.ac.in>
2026-02-01 14:25:55 +0000

    Sync LeetCode submission - Angle Between Hands of a Clock (java)

---
+++ problems/angle_between_hands_of_a_clock/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double angleClock(int hour, int minutes) {
+
+        double hourAngle = 30 * hour + 0.5 * minutes;
+        double minuteAngle = 6 * minutes;
+
+        double angle = Math.abs(hourAngle - minuteAngle);
+
+        return Math.min(angle, 360 - angle);
+    }
+}
