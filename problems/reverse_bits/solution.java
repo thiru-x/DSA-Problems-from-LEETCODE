62b32c3c5ceb678e0393bb1d8fabeb59d88dcc38
Thirugnanam N <23csb53@karpagamtech.ac.in>
2026-02-16 03:48:41 +0000

    Sync LeetCode submission - Reverse Bits (java)

--- problems/reverse_bits/solution.java
+++ problems/reverse_bits/solution.java
@@ -1,12 +1,8 @@
-public class Solution {
-    // you need treat n as an unsigned value
+class Solution {
     public int reverseBits(int n) {
-          int result = 0;
-        for (int i = 0; i < 32; i++) {
-            result <<= 1;         
-            result |= (n & 1);  
-            n >>= 1;             
-        }
+        
+        int result = Integer.reverse(n);
+
         return result;
     }
 }
