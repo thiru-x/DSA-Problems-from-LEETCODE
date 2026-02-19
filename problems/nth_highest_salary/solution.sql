a4e8f9434d26259308600434c107184ce07220fd
Thirugnanam N <23csb53@karpagamtech.ac.in>
2026-02-19 06:20:29 +0000

    Sync LeetCode submission - Nth Highest Salary (mysql)

---
+++ problems/nth_highest_salary/solution.sql
@@ -0,0 +1,14 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+      # Write your SQL Query here
+    SELECT salary
+    FROM (
+        SELECT salary,
+               DENSE_RANK() OVER (ORDER BY salary DESC) AS rnk
+        FROM Employee
+    ) t
+    WHERE rnk = N
+    LIMIT 1
+  );
+END;
