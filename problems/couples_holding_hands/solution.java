f6acf3c665e8ce9b3491c77f3b7536983651e0f7
Thirugnanam N <23csb53@karpagamtech.ac.in>
2026-06-15 04:00:07 +0000

    Sync LeetCode submission - Couples Holding Hands (java)

--- problems/couples_holding_hands/solution.java
+++ problems/couples_holding_hands/solution.java
@@ -1,39 +1,42 @@
-// Java
 class Solution {
     public int minSwapsCouples(int[] row) {
-        int swaps = 0;
         int n = row.length;
+        int swaps = 0;
+
+        // position[i] = current seat of person i
         int[] position = new int[n];
 
-        // Map each person to their seat index
         for (int i = 0; i < n; i++) {
             position[row[i]] = i;
         }
 
-        // Process pairs of seats
         for (int i = 0; i < n; i += 2) {
-            int first = row[i];
-            int second = row[i + 1];
-            int expectedSecond = first ^ 1; // Partner of first person
 
-            if (second != expectedSecond) {
-                swaps++;
-                int partnerIndex = position[expectedSecond];
+            int firstPerson = row[i];
+            int partner = firstPerson ^ 1;
 
-                // Swap second person with the correct partner
-                swap(row, i + 1, partnerIndex);
-
-                // Update position map after swap
-                position[second] = partnerIndex;
-                position[expectedSecond] = i + 1;
+            // If partner already beside firstPerson
+            if (row[i + 1] == partner) {
+                continue;
             }
-        }
-        return swaps;
-    }
 
-    private void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+            // Current position of partner
+            int partnerPos = position[partner];
+
+            // Person sitting at i+1
+            int wrongPerson = row[i + 1];
+
+            // Swap
+            row[i + 1] = partner;
+            row[partnerPos] = wrongPerson;
+
+            // Update positions
+            position[partner] = i + 1;
+            position[wrongPerson] = partnerPos;
+
+            swaps++;
+        }
+
+        return swaps;
     }
 }
