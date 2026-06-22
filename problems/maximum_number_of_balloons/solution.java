d8f1882b89d4ab23aea34d20158f36a307303639
Thirugnanam N <23csb53@karpagamtech.ac.in>
2026-06-22 07:32:55 +0000

    Sync LeetCode submission - Maximum Number of Balloons (java)

---
+++ problems/maximum_number_of_balloons/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        
+        int b=0,a=0,l=0,o=0,n=0;
+
+        for(char ch:text.toCharArray()){
+            
+            if(ch=='b') b++;
+            if(ch=='a') a++;
+            if(ch=='l') l++;
+            if(ch=='o') o++;
+            if(ch=='n') n++;
+        }
+            return Math.min(Math.min(b,a),Math.min(Math.min(l/2,o/2),n));
+    }
+}
